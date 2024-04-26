745ab31b7916f0d593e5bda17c042467384e9e0f
aggbhh20 <aggbhh4@gmail.com>
2024-04-26 21:46:39 +0300

    Made a different html for viewing blogposts.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("fuckyou", views.fuckyou, name="fuckyou"),
     path("todos/", views.todos, name="todos"),
     path("blogposts/", views.blogposts, name="blogposts"),
-    path("deleteBlogpost/", views.deleteBlogpost, name="deleteBlogpost")
+    path("deleteBlogpost/", views.deleteBlogpost, name="deleteBlogpost"),
+    path("seeBlogposts/", views.seeBlogposts, name="seeBlogposts")
 ]
